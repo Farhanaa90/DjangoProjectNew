0f8ab438c7c811e632e5657715ab6b994e1dc20e
CodeByRedwan <ambitiousredwan12@gmail.com>
2025-10-23 17:43:38 +0600

    Created forms.py and write few codes for that

---
+++ Share4Care/users/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import UserProfile, Message
+
+
+class RegistrationForm(forms.ModelForm):
+    name = forms.CharField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput())
+    user_type = forms.ChoiceField(choices=[('donor', 'Donor'), ('collector', 'Collector')])
+
+    class Meta:
+        model = User
+        fields = ['name', 'email', 'username', 'password', 'user_type']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    name = forms.CharField(max_length=100, required=False)
+    email = forms.EmailField(required=False)
+
+    class Meta:
+        model = UserProfile
+        fields = ['phone', 'address', 'city']
